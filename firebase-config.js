98408609e3c18a2d0fc4262802a4f073c25867c6
John Rey Marquillero <johnreymarquillero@gmail.com>
2026-04-12 11:30:26 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js"; // Added Storage Import
 
 const firebaseConfig = {
   apiKey: "AIzaSyC8xAbXv9mzChkYHfEIePU0H-22UnZq-Ew",
   authDomain: "it-class-app.firebaseapp.com",
   projectId: "it-class-app",
-  storageBucket: "it-class-app.firebasestorage.app",
+  storageBucket: "it-class-app.firebasestorage.app", // This links to your storage
   messagingSenderId: "834373641008",
   appId: "1:834373641008:web:6e0f767dfba2bb64cb1ef5",
   measurementId: "G-TEF9WXSMLX"
@@ -14,8 +15,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export Services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app); // Added Storage Export
 
 // Helper function to save data easily
 export const saveToDatabase = async (collectionName, data) => {
